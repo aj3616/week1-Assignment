6b67e0d7b6959c1452b7b58e51d4e3270a55783b
amyjin <amyzh.jin@mail.utoronto.ca>
2023-09-15 16:02:07 -0400

    915

---
+++ Multiples.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Multiples {
+    public static int main(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 | i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
